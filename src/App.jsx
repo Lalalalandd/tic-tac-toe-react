ab60ab0337bbf6666b87420b0eee38c387935f98
Jefri Achmad Maulana <jefriachmadm@gmail.com>
2024-08-28 20:12:52 +0700

    make status for next player and winner

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const [squares, setSquares] = useState(Array(9).fill(null));
 const [xIsNext, setXIsNext] = useState(true);
 
 function handleClick(i) {
-  if (squares[i]) return;
+  if (squares[i] || calculateWinner(squares) ) return;
   const nextSquares = squares.slice();
   
   nextSquares[i] = xIsNext ? "X" : "O"
@@ -19,8 +19,17 @@ function handleClick(i) {
   setXIsNext(!xIsNext);
 }
 
+const winner = calculateWinner(squares);
+let status = '';
+if (winner) {
+  status = 'Winner: ' +winner;
+} else {
+  status = 'Next player' + (xIsNext ? 'X' : 'O');
+}
+
   return (
-    
+    <>
+    <div className='status'>{status}</div>
     <div className='board'>
      <Square value={squares[0]} onSquareClick={() =>handleClick(0)}/>
      <Square value={squares[1]} onSquareClick={() =>handleClick(1)} />
@@ -32,6 +41,28 @@ function handleClick(i) {
      <Square value={squares[7]} onSquareClick={() =>handleClick(7)} />
      <Square value={squares[8]} onSquareClick={() =>handleClick(8)} />
      </div>
+     </>
   )
 }
 
+function calculateWinner(squares){
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 8],
+
+  ];
+
+  for(let i=0; i < lines.length; i++){
+    const [a,b,c] = lines[i];
+
+    if (squares[a] && squares[a] === squares[b] && squares[c])
+      return squares[a];
+    }
+    return false;
+}
