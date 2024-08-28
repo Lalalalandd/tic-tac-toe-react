09fa8b7b1dda8ee305282764c99796d21a0da808
Jefri Achmad Maulana <jefriachmadm@gmail.com>
2024-08-28 19:39:09 +0700

    set litting up

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,31 @@
 import { useState } from 'react'
 
-function Square() {
-  const [value, setValue] = useState('');
+function Square({value, onSquareClick}) {
 
-  function handleClick() {
-    setValue('X');
-  }
-
-  return <button className='square' onClick={handleClick}>{ value }</button>
+  return <button className='square' onClick={onSquareClick}>{ value }</button>
 }
 
 export default function Board() {
+const [squares, setSquares] = useState(Array(9).fill(null));
+
+function handleClick(i) {
+  const nextSquares = squares.slice();
+  nextSquares[i] = 'X';
+  setSquares(nextSquares);
+}
 
   return (
     
     <div className='board'>
-     <Square />
-     <Square />
-     <Square />
-     <Square />
-     <Square />
-     <Square />
-     <Square />
-     <Square />
-     <Square />
+     <Square value={squares[0]} onSquareClick={() =>handleClick(0)}/>
+     <Square value={squares[1]}/>
+     <Square value={squares[2]}/>
+     <Square value={squares[3]}/>
+     <Square value={squares[4]}/>
+     <Square value={squares[5]}/>
+     <Square value={squares[6]}/>
+     <Square value={squares[7]}/>
+     <Square value={squares[8]}/>
      </div>
   )
 }
